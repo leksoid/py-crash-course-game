c0f8e887706abd5b04e6c3de449fe9b3a6c1421f
leksoid <aliaksei.taran@gmail.com>
2020-01-15 23:00:09 -0500

    Add background color

--- alien-invasion.py
+++ alien-invasion.py
@@ -11,14 +11,20 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
-        
+
+        # Set the background color
+        self.bg_color = (230, 230, 230)
+
     def run_game(self):
         """Start the main game loop"""
         while True:
-            # Watch for keyboard and mouse events
+            # Watch for keyboard and mouse events. Quit is the "close" button
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            # Redraw the screen during each pass through the loop
+            self.screen.fill(self.bg_color)
+
             # Make the recently drawn screen visible
             pygame.display.flip()
 
